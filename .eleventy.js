47706a97cda904e92f53847c25f5820ec416147a
fynn.lehnert <f@fynn.it>
2020-09-26 15:39:30 +0200

    news mit bilder

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setTemplateFormats("html, liquid, njk, md");
     eleventyConfig.addPassthroughCopy({"assets/": "/assets/"});
     eleventyConfig.addWatchTarget("./scss/");
+
     eleventyConfig.setFrontMatterParsingOptions({
       excerpt: true,
+      excerpt_separator: "---"
     });
 
     eleventyConfig.addFilter("dateformat", function(dateIn) {
